205e2ba0fb1d4e2e8b87984a74a3e1c8721f7885
foxking0416 <foxking0416@gmail.com>
2015-08-20 15:37:51 +0800

    add sort function

--- spring/js/main.js
+++ spring/js/main.js
@@ -6,7 +6,7 @@
 		cw = cw * 0.3;
 		$('.header').css({'height':cw+'px'});
 	});
-	
+
 	//For switching banner 
 	var banner=new Array();   
 	var adNum=0;  
@@ -31,31 +31,22 @@
 			}
 			
 			//Used longitude and latitude to locate the pointer
-			var shiftX = 39903-332.74*projectList[i].longitude;//40105 - 336.16*projectList[i].longitude;  
-			var shiftY = 343.48*projectList[i].latitude - 8456.9; //(projectList[i].latitude - 24.669) / 0.0029
-			var scaleX = projectList[i].scaleX;
-			var scaleY = projectList[i].scaleY;
-			
-			
-			
-			
-			//positionCollection[projectList[i].linkPath]=[projectList[i].posX, projectList[i].posY, projectList[i].scaleX, projectList[i].scaleY]; 
-			positionCollection[projectList[i].linkPath]=[shiftX, shiftY, projectList[i].scaleX, projectList[i].scaleY]; 
+			var shiftX = 39903-332.74*projectList[i].longitude;
+			var shiftY = 343.48*projectList[i].latitude - 8456.9; 
+
+			positionCollection[projectList[i].linkPath]=[shiftX, shiftY, 900, 1170]; 
 		}
 		
 		for(i = 0; i < projectList.length; i++){
 			if(projectList[i].display === 'True'){
 				var pageName = projectList[i].linkPath;
-				if(projectList[i].detail === 'True'){
+
 					$(".project_list").append('<div id= '+pageName+' class="divAutoAdd"><p>'+projectList[i].startYear+'年_'+projectList[i].name+'</p></div>');
 					$('#' + pageName).prepend(projectListPic[i]);
 					$('#' + pageName).click(function(){ 
 						jumpToProjectPage( $(this).context.id );
 					});
-				}
-				else{
-					$(".project_list").append('<div id= '+pageName+' class="divAutoAddWithoutDetail"><p>'+projectList[i].startYear+'年_'+projectList[i].name+'</p>  </div>');
-				}
+
 				
 				$('#' + pageName).hover(function () {
 					var projectId = $(this).context.id;
@@ -68,10 +59,6 @@
 					$('.region_select').css('background-size', 300 + 'px ' + 390 + 'px');
 					$(".region_select img").toggle("blind", {}, 10);
 				});
-				
-				
-
-				
 			}
 		}		
   });
@@ -152,7 +139,7 @@
  
  	function jumpFromProjectToOther(destination){
 		sessionStorage.setItem('session', destination);
-		window.location.href='index.html';
+		window.location.href='home.html';
 	};
 	
 	function detectBrowser(){
@@ -165,6 +152,229 @@
 		this.isCh = (sAgent.indexOf("chrome")!=-1);//chrome
 		this.isMa = this.isIE;//marthon
 		this.isOther = (!this.isIE && !this.isFF && !this.isSa && !this.isOp && !this.isNN && !this.isSa);//unknown Browser
+	};
+	
+	function valueExistence(list, value) {
+		for (var i = 0; i < list.length; i++) {
+			if (list[i] === value) {
+				return true;
+			}
+		}
+		return false;
+	};
+	
+	function resortProjects(subClassSelectedValue){
+		switch (subClassSelectedValue){
+			case "101"://依年份_由近往前
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "102"://依年份_由前往近
+				for(i = projectList.length-1; i >= 0; i--){
+					if(projectList[i].display === 'True'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "201"://依金額_
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "202"://依金額_
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].amount < 5000){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "203"://依金額_
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].amount > 5000){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "301"://依類型_全部
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' ){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "302"://依類型_建築
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 1 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "303"://依類型_景觀
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 2 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "304"://依類型_裝修
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 3 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "305"://依類型_廠房
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 4 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "306"://依類型_土木
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 5 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "307"://依類型_運動設施
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 6 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "308"://依類型_木造
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 7 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "309"://依類型_廟宇建築
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 8 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "310"://依類型_古蹟修復
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 9 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "311"://依類型_綠建築
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && valueExistence( projectList[i].type, 10 )){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "401"://依地區_全部
+				for(i = 0; i < projectList.length; i++){
+					createAutoDiv(i);
+				}
+			break;
+			case "402"://依地區_台北
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '台北'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "403"://依地區_苗栗
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '苗栗'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "404"://依地區_南投
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '南投'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "405"://依地區_嘉義
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '嘉義'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "406"://依地區_台南
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '台南'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "407"://依地區_高雄
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '高雄'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "408"://依地區_屏東
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '屏東'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "409"://依地區_台東
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '台東'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "410"://依地區_澎湖
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].region === '澎湖'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+			case "501"://重點工程
+				for(i = 0; i < projectList.length; i++){
+					if(projectList[i].display === 'True' && projectList[i].important === 'high'){
+						createAutoDiv(i);
+					}
+				}
+			break;
+		}
+	};
+	
+	function createAutoDiv(i){
+		var pageName = projectList[i].linkPath;
+
+		$(".project_list").append('<div id= '+pageName+' class="divAutoAdd"><p>'+projectList[i].startYear+'年_'+projectList[i].name+'</p></div>');
+		$('#' + pageName).prepend(projectListPic[i]);
+		$('#' + pageName).click(function(){ 
+			jumpToProjectPage( $(this).context.id );
+		});
+
+
+		$('#' + pageName).hover(function () {
+			var projectId = $(this).context.id;
+			$('.region_select').css('background-position', positionCollection[projectId][0] + 'px ' + positionCollection[projectId][1] + 'px');	
+			$('.region_select').css('background-size', positionCollection[projectId][2] + 'px ' + positionCollection[projectId][3] + 'px');
+			setTimeout(function(){ $(".region_select img").toggle("blind", {}, 50)}, 1500);
+		}, function () {
+			$('.region_select').css('background-position', 0 + 'px ' + 0 + 'px');
+			$('.region_select').css('background-size', 300 + 'px ' + 390 + 'px');
+			$(".region_select img").toggle("blind", {}, 10);
+		});
 	};
 	
 	
